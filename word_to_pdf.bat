91771b1f31479b81e775db9392f533b3f6d426d2
Kyle Jones <51127919+kylemjonesislanders@users.noreply.github.com>
2022-03-18 11:24:04 -0500

    Create word_to_pdf.bat

---
+++ word_to_pdf.bat
@@ -0,0 +1,6 @@
+:: move to directory that has python script
+cd "C:\path\to\python_scripts"
+:: run python script
+"C:\Program Files (x86)\Python38-32\python" -m word_to_pdf
+:: keep windows cmd prompt open for 10 seconds after task completes
+TIMEOUT 10
